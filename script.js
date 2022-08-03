0b1a31cd325ebeaaf31a17df4c7a13cd3a36b01a
Thomas <tho.daskalakis@gmail.com>
2022-08-04 02:25:46 +0300

    Create New Grid button
    The button offers the user the option to create a new grid of chosen
    size (square), also clears the canvas.

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const container = document.querySelector('#container');
 
+function setGridSize(size=4) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-// Create divs in container
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.id = i + 1;
-    container.appendChild(div);
+    // Create divs in container
+    for (let i = 0; i < size * size; i++) {
+        let div = document.createElement('div');
+        div.id = i + 1;
+        container.appendChild(div);
+    }
+
+    const grid = document.querySelectorAll('div#container div');
+
+    grid.forEach(
+        box => box.addEventListener('mouseenter', trail, {once: true}));
 }
 
-const grid = document.querySelectorAll('div#container div');
-console.log(grid);
+setGridSize();
 
-grid.forEach(box => box.addEventListener('mouseenter', trail, {once: true}));
+const btn = document.querySelector('#btn');
+// console.log(btn);
+btn.addEventListener('click', function(e) {
+    let dim = prompt("Please enter the dimension for the new grid (1-100).");
+    if (dim > 100) alert("Please input a value less than 100.");
+    if (dim < 1) alert("Cannot have non-positive values.");
+
+    const children = container.querySelectorAll('div');
+    children.forEach(child => container.removeChild(child));
+    setGridSize(dim);
+});
+
+// console.log(grid);
+
 
 function trail(e) {
-    console.log(e.target);
+    // console.log(e.target);
     this.classList.add('trail');
 }
